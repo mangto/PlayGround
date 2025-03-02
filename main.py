6016db9c0fe675500f89571566d369ef1e126c08
unknown <mangto0701@gmail.com>
2025-03-02 23:42:00 +0900

    Rah

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame as pg
+import sys
+
+pg.init()
+
+# F = G*(m_1 * m_2) / r
+
+window = pg.display.set_mode((1280, 720))
+Clock = pg.time.Clock()
+
+class PhysicalField:
+    def __init__(self):
+        pass
+
+    def render(self):
+        return
+    
+while __name__ == "__main__":
+    
+    Clock.tick(60)
+    pg.display.update()
